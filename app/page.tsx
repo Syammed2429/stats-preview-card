522330c87152fe61c0760bc437b35ee1dcfd4342
Dada Khalandar <kerry.blig12@gmail.com>
2023-12-18 01:12:18 +0800

    fixed the alignments

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Stats } from '@/components/stats/stats';
 
 export default function Home() {
   return (
-    <div className='flex justify-center items-center h-screen'>
+    <div className='flex justify-center items-center h-screen p-[5.5rem_1.5rem]'>
       <Stats />
     </div>
   );
