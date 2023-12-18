8d276d6afc5c2c68824b4b62ffcb379764605c4c
Dada Khalandar <kerry.blig12@gmail.com>
2023-12-18 09:52:57 +0800

    redesigned the layout using the grid

--- components/stats/stats.tsx
+++ components/stats/stats.tsx
@@ -21,8 +21,8 @@ export const Stats = () => {
   ];
 
   return (
-    <div className='flex  flex-col-reverse justify-center items-center  mt-[25rem] xl:mt-0  xl:justify-normal xl:items-stretch h-[100dvh] w-auto  xl:flex-row  xl:w-[60rem] xl:h-[27rem] bg-[#1B1937] shadow-[0px_20px_20px_-10px_rgba(23,25,41,0.20)] rounded-lg '>
-      <div className='flex flex-col justify-center items-center pt-[2rem] xl:my-20 space-y-8 xl:space-y-10 w-auto md:w-[350px]  xl:w-[400px] mx-20 text-center xl:text-left'>
+    <div className='flex  flex-col-reverse justify-center items-center  mt-[15rem] xl:mt-0  xl:grid xl:grid-cols-6  h-[100dvh] w-auto  xl:flex-row  xl:w-[70rem]  xl:h-[30rem]  bg-[#1B1937] shadow-[0px_20px_20px_-10px_rgba(23,25,41,0.20)] rounded-lg '>
+      <div className='flex flex-col justify-center items-center pt-[2rem] xl:pt-0  space-y-8 xl:space-y-10 w-auto md:w-[400px]  xl:col-span-3  xl:place-self-center place-items-center text-center xl:text-left'>
         <div className='text-4xl font-bold  xl:w-full'>
           Get <span className='text-[#AB5CDB]'>insights </span>that help your
           business grow.
@@ -31,7 +31,7 @@ export const Stats = () => {
           Discover the benefits of data analytics and make better decisions
           regarding revenue, customer experience, and overall efficiency.
         </div>
-        <div className='flex flex-col xl:flex-row gap-5 xl:gap-24 h-full pb-28 xl:pb-0'>
+        <div className='flex flex-col xl:flex-row gap-5 xl:gap-24 h-full pb-20 md:pb-28 xl:pb-0'>
           {statsData?.map((stat) => (
             <div key={stat?.id} className='space-y-2'>
               <div className='text-2xl font-bold'>{stat?.value}</div>
@@ -43,12 +43,11 @@ export const Stats = () => {
         </div>
       </div>
 
-      <div className=' bg-[#9f44d8] rounded-tr-lg rounded-br-lg '>
+      <div className='xl:col-span-3 rounded-tr-lg rounded-br-lg bg-[#9f44d8]  xl:h-[30rem] mb-9'>
         <Image
           src={bitMap}
           alt='some image'
-          objectFit='cover'
-          className=' mix-blend-multiply h-full'
+          className='mix-blend-multiply xl:h-[30rem] object-cover xl:w-full'
         />
       </div>
     </div>
